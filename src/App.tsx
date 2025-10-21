427b2b9a0e51b27c256aafe0add1e04c76f17e64
miguelangeldch <miguelangel.carvallo@galup.com.ve>
2025-10-21 13:32:32 -0400

    prueba tecnica

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,34 @@ import { TaskList } from "./components/TaskList";
 import { TaskForm } from "./components/TaskForm";
 import { FilterPanel } from "./components/FilterPanel";
 import { Summary } from "./components/Summary";
+import { getTasks } from "./api/tasks";
+import { useEffect, useState } from "react";
+import type { Task } from "./interfaces/Tasks.interfaces";
 
 function App() {
+  const [data, setData] = useState<Task[]>([])
+
+  const handleFetchTask = async () => {
+    const res = await getTasks()
+
+    if (!res) {
+      return
+    }
+
+    setData(res)
+  }
+
+  useEffect(() => {
+    handleFetchTask()
+  }, [])
+
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Task Manager SSR</h1>
       <TaskForm />
       <hr />
       <FilterPanel />
-      <TaskList tasks={[]} />
+      <TaskList tasks={data} />
       <Summary total={0} completed={0} />
     </div>
   );
--- src/api/tasks.ts
+++ src/api/tasks.ts
@@ -3,12 +3,49 @@ import { type Task } from "../interfaces/Tasks.interfaces";
 const BASE_URL = "http://localhost:3000/api";
 
 export const getTasks = async (): Promise<Task[]> => {
+
+  try {
+    const tasks = await fetch(`${BASE_URL}/tasks`, {
+      method: 'GET',
+
+    })
+
+    if (!tasks.ok) {
+      throw new Error("No fue posible cargar tareas")
+    }
+
+    const data = await tasks.json()
+
+    return data
+
+  } catch (error) {
+
+    console.error({ error })
+
+  }
+
   // TODO: completar fetch GET
   return Promise.reject("getTasks no implementado");
 };
 
 export const createTask = async (task: Omit<Task, "id">): Promise<Task> => {
   // TODO: completar fetch POST
+
+  try {
+
+    const submit = await fetch(`${BASE_URL}/tasks/create-tasks`, {
+      method: 'POST',
+      body: JSON.stringify(task)
+    })
+
+    if (!submit.ok) throw new Error('No se pudo crear la tarea')
+
+    return submit.json()
+
+  } catch (error) {
+    console.error({ error })
+  }
+
   return Promise.reject("createTask no implementado");
 };
 
